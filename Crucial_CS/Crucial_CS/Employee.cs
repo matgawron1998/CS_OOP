7fc3059402d2cdbbf015a5c5f0c31c1352863f73
[Mateusz] <[mateusz.gawron1998@gmail.com]>
2022-10-22 09:59:35 +0200

    added new file Crucial_CS for new course day

---
+++ Crucial_CS/Crucial_CS/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crucial_CS
+{
+    internal class Employee
+    {
+        public static string NewEmployee;
+        public static int EmployeeCounter;
+
+        public string Id;
+        public string Name;
+
+        public Employee(string name, string id)
+        { 
+            Name = name;
+            Id = id;
+            ++EmployeeCounter;
+        }
+
+        public static void AssignNewEmployee(string newEmployee)
+        { 
+            NewEmployee = newEmployee;
+        }
+
+        public void DisplayEmployee()
+        {
+            Console.WriteLine($"{Name}: {Id}");
+        }
+    }
+}
---
+++ Crucial_CS/Crucial_CS/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Crucial_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee mateusz = new Employee("Mateusz", "Mate");
+            mateusz.DisplayEmployee();
+
+            Employee.AssignNewEmployee(mateusz.Id);
+
+            Console.WriteLine($"Number of Employees: {Employee.EmployeeCounter}");
+            Console.WriteLine($"New Employee: {Employee.NewEmployee}");
+
+            Employee marek = new Employee("Marek", "Maro");
+            marek.DisplayEmployee();
+
+            Employee.AssignNewEmployee(marek.Id);
+
+            Console.WriteLine($"Number of Employees: {Employee.EmployeeCounter}");
+            Console.WriteLine($"New Employee: {Employee.NewEmployee}");
+        }
+    }
+}
