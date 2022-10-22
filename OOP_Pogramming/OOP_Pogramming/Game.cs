9bbc4c890c58c89f547c8fc68d3b1ea7ab448719
[Mateusz] <[mateusz.gawron1998@gmail.com]>
2022-10-22 06:53:13 +0200

    Added Game class and Game task in Installer

---
+++ OOP_Pogramming/OOP_Pogramming/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Pogramming
+{
+    internal class Game
+    {
+        public string Name;
+        public int ProductionYear;
+        public int Price;
+
+        public void DisplayPrice()
+        {
+            Console.WriteLine($"The price of this game ios {Price}");
+        }
+
+        public void DisplayContents() 
+        {
+            Console.WriteLine($"Name of this game is {Name}, production year is {ProductionYear}");
+        }
+    }
+}
--- OOP_Pogramming/OOP_Pogramming/Installer.cs
+++ OOP_Pogramming/OOP_Pogramming/Installer.cs
@@ -9,8 +9,23 @@ namespace OOP_Pogramming
     internal class Installer
     {
         public static void Game()
-        { 
-        
+        {
+            Game Cyber = new Game();
+            Game Diablo4 = new Game();
+
+            Cyber.Name = "Cyberpunk 2077";
+            Cyber.ProductionYear = 2020;
+            Cyber.Price = 199;
+
+            Diablo4.Name = "Diablo 4";
+            Diablo4.ProductionYear = 2023;
+            Diablo4.Price = 299;
+
+            Cyber.DisplayPrice();
+            Cyber.DisplayContents();
+
+            Diablo4.DisplayPrice();
+            Diablo4.DisplayContents();
         }
     }
 }
