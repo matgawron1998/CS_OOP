5eb093aa5c5a4fca7a6380f660473e2c70284226
[Mateusz] <[mateusz.gawron1998@gmail.com]>
2022-10-22 06:39:01 +0200

    Adding programs to GitHub

---
+++ OOP_Pogramming/OOP_Pogramming/Installer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Pogramming
+{
+    internal class Installer
+    {
+        public static void Game()
+        { 
+        
+        }
+    }
+}
---
+++ OOP_Pogramming/OOP_Pogramming/Program.cs
@@ -0,0 +1,28 @@
+﻿using OOP_Pogramming;
+using System;
+​
+namespace OOP_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string methodName = "";
+
+            while (true)
+            {
+                Console.WriteLine("\nWrite name of method u want to run or press \"x\" to exit");
+                methodName = Console.ReadLine();
+                if (methodName == "x" || methodName == "X")
+                    break;
+                else 
+                {
+                    Console.WriteLine("\n-----------------");
+                    Installer installer = new Installer();
+                    installer.GetType().GetMethod(methodName)?.Invoke(installer, null);
+                    Console.WriteLine("\n-----------------");
+                }
+            }
+        }
+    }
+}
